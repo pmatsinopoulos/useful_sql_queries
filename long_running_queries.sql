c7ccea8ae176783b82f180ce66a21479c8669435
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-01-26 14:26:24 +0200

    Get size of table on disk

--- long_running_queries.sql
+++ long_running_queries.sql
@@ -9,3 +9,6 @@ SELECT
 FROM pg_stat_activity
 WHERE (now() - pg_stat_activity.query_start) > interval '5 minutes';
 -----------------------------------------------------------------------
+-- Get the size of a table on disk
+SELECT pg_size_pretty(pg_total_relation_size('"<schema>"."<table>"'));
+-----------------------------------------------------------------------
