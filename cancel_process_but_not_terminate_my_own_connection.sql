381be79b1f1bbd92aa9c91df6c23c0ef81c86420
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-02-01 18:40:36 +0200

    Update cancel_process_but_not_terminate_my_own_connection.sql

--- cancel_process_but_not_terminate_my_own_connection.sql
+++ cancel_process_but_not_terminate_my_own_connection.sql
@@ -1,3 +1,8 @@
 SELECT pg_cancel_backend(pid)
 FROM pg_stat_activity
 WHERE pid <> pg_backend_pid();
+
+-- If first option does not work, this is a bit more forceful may affect additional queries being ran and the connection may be reset
+SELECT pg_terminate_backend(pid)
+FROM pg_stat_activity
+WHERE pid <> pg_backend_pid();
