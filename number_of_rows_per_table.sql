81b4b39fcd2fea35f4de55bb166f98259d0b4028
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-10-14 13:41:10 +0300

    Create number_of_rows_per_table.sql

---
+++ number_of_rows_per_table.sql
@@ -0,0 +1,10 @@
+SELECT 
+    schemaname,
+    relname AS table_name,
+    n_live_tup AS row_count
+FROM 
+    pg_stat_user_tables
+WHERE 
+    schemaname = 'public'
+ORDER BY 
+    n_live_tup DESC;
